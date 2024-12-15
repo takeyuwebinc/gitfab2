aa1ce667e050ff6171b40b4ad1f17cb222149611
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-12-15 07:58:10 +0000

    fix: sentry-raven から sentry-rails に更新
    より新しいバージョンに差し替え
    使用していないScoutAPMなどを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   unless Rails.env.development?
-    rescue_from StandardError, with: :render_500
+    rescue_from Exception, with: :render_500
     rescue_from ActiveRecord::RecordNotFound, with: -> { render_404 }
     rescue_from ActionController::RoutingError, with: -> { render_404 }
   end
   rescue_from CanCan::AccessDenied, with: -> { render_401 }
 
   after_action :store_location
-  before_action :set_raven_context
+  before_action :set_sentry_context
 
   def current_user
     @current_user ||= begin
@@ -52,18 +52,18 @@ class ApplicationController < ActionController::Base
         exception.backtrace.each do |f|
           Rails.logger.error("  #{f}")
         end
+        Sentry.capture_exception(exception) # TODO: Rails 7.0 になったら消す ActiveSupport::ErrorReporter でサブスクライブされているため
       end
-      env = request.env
-      method = env['REQUEST_METHOD']
-      path = env['REQUEST_PATH']
-      user_agent = env['HTTP_USER_AGENT']
-      message = "#{method} '#{path}' \n UA: '#{user_agent}'"
-      ExceptionNotifier.notify_exception exception, env: env, data: { message: message }
       render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
     end
 
-    def set_raven_context
-      Raven.user_context(id: current_user&.id)
-      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+    def set_sentry_context
+      Sentry.set_user(
+        id: current_user&.id,
+        ip_address: request.remote_ip,
+      )
+      Sentry.set_extras(
+        params: params.to_unsafe_h,
+      )
     end
 end
