41f4519a4b71a60e129d4edbc1cb3d6eff7992bc
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-12-15 06:45:21 +0000

    fix: config.load_defaults = 6.0

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require_relative "../lib/middlewares/maintenance_mode"
 module Gitfab2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     config.time_zone = 'Tokyo'
     config.active_job.queue_adapter = :delayed_job
