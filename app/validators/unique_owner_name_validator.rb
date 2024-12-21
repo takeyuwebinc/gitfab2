963852229c941bf74fbaf0e8df081633f765af3d
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-12-21 05:03:54 +0000

    fix: deprecation warnings
    Calling  << to an ActiveModel::Errors message array in order to add an error is deprecated.

--- app/validators/unique_owner_name_validator.rb
+++ app/validators/unique_owner_name_validator.rb
@@ -1,12 +1,12 @@
 class UniqueOwnerNameValidator < ActiveModel::EachValidator
   def validate(record)
     unless record.name
-      record.errors[:name] << ' is nil'
+      record.errors.add(:name, ' is nil')
       return
     end
     slug = record.normalize_friendly_id(record.name)
     if User.where(slug: slug).where.not(id: record.id).exists? || Group.where(slug: slug).where.not(id: record.id).exists?
-      record.errors[:name] << 'has already been taken.'
+      record.errors.add(:name, 'has already been taken.')
     end
   end
 end
