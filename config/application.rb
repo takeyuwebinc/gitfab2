86cb5e17147664a1be2fcc8d288486911c56677a
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-12-27 06:42:45 +0000

    fix: 日本限定をいったん外す

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 require_relative "../lib/core_ext/warning"
-require_relative "../lib/middlewares/jp_only"
+# require_relative "../lib/middlewares/jp_only"
 require_relative "../lib/middlewares/maintenance_mode"
 
 module Gitfab2
@@ -23,7 +23,7 @@ module Gitfab2
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.middleware.insert 0, JpOnly
+    # config.middleware.insert 0, JpOnly
     config.middleware.insert 0, MaintenanceMode
   end
 end
