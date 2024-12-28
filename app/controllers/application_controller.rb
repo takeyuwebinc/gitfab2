d28cff2d3985d3f07fea527e52a6ce8f1de44b0f
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-12-28 05:59:08 +0000

    fix: invalid requests
    本番環境でテンプレートのないcontent-typeのリクエスト（たとえば /fabble.gz のような）は not found を返す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
     rescue_from Exception, with: :render_500
     rescue_from ActiveRecord::RecordNotFound, with: -> { render_404 }
     rescue_from ActionController::RoutingError, with: -> { render_404 }
+    rescue_from ActionView::MissingTemplate, with: -> { render_404 }
   end
   rescue_from CanCan::AccessDenied, with: -> { render_401 }
 
