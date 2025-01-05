75b6a9695a577c97a3569d6c5a4c436e15f5e51c
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2025-01-05 03:06:14 +0000

    fix: rails app:update

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,26 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
+# This configuration file will be evaluated by Puma. The top-level methods that
+# are invoked here are part of Puma's configuration DSL. For more information
+# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+
+# Puma starts a configurable number of processes (workers) and each process
+# serves each request in a thread from an internal thread pool.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# The ideal number of threads per worker depends both on how much time the
+# application spends waiting for IO operations and on how much you wish to
+# to prioritize throughput over latency.
+#
+# As a rule of thumb, increasing the number of threads will increase how much
+# traffic a given process can handle (throughput), but due to CRuby's
+# Global VM Lock (GVL) it has diminishing returns and will degrade the
+# response time (latency) of the application.
+#
+# The default is set to 3 threads as it's deemed a decent compromise between
+# throughput and latency for the average Rails application.
+#
+# Any libraries that use a connection pool or another resource pool should
+# be configured to provide at least as many connections as the number of
+# threads. This includes Active Record's `pool` parameter in `database.yml`.
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
@@ -14,15 +30,15 @@ threads min_threads_count, max_threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
 port        ENV.fetch("PORT") { 3001 }
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# Specify the PID file. Defaults to tmp/pids/server.pid in development.
+# In other environments, only set the PID file if requested.
+pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -39,33 +55,32 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # preload_app!
 
-# Allow puma to be restarted by `rails restart` command.
+# Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
 if Rails.env.development?
-    key_file = Rails.root.join("config", "certs", "localhost.key")
-    cert_file = Rails.root.join("config", "certs", "localhost.cert")
-  
-    unless key_file.exist?
-      root_key = OpenSSL::PKey::RSA.new(2048)
-      key_file.write(root_key)
-  
-      root_cert = OpenSSL::X509::Certificate.new.tap do |root_ca|
-        root_ca.version = 2 # cf. RFC 5280 - to make it a "v3" certificate
-        root_ca.serial = 0x0
-        root_ca.subject = OpenSSL::X509::Name.parse "/C=BE/O=A1/OU=A/CN=localhost"
-        root_ca.issuer = root_ca.subject # root CA"s are "self-signed"
-        root_ca.public_key = root_key.public_key
-        root_ca.not_before = Time.now
-        root_ca.not_after = root_ca.not_before + 2 * 365 * 24 * 60 * 60 # 2 years validity
-        root_ca.sign(root_key, OpenSSL::Digest::SHA256.new)
-      end
-      cert_file.write(root_cert)
+  key_file = Rails.root.join("config", "certs", "localhost.key")
+  cert_file = Rails.root.join("config", "certs", "localhost.cert")
+
+  unless key_file.exist?
+    root_key = OpenSSL::PKey::RSA.new(2048)
+    key_file.write(root_key)
+
+    root_cert = OpenSSL::X509::Certificate.new.tap do |root_ca|
+      root_ca.version = 2 # cf. RFC 5280 - to make it a "v3" certificate
+      root_ca.serial = 0x0
+      root_ca.subject = OpenSSL::X509::Name.parse "/C=BE/O=A1/OU=A/CN=localhost"
+      root_ca.issuer = root_ca.subject # root CA"s are "self-signed"
+      root_ca.public_key = root_key.public_key
+      root_ca.not_before = Time.now
+      root_ca.not_after = root_ca.not_before + 2 * 365 * 24 * 60 * 60 # 2 years validity
+      root_ca.sign(root_key, OpenSSL::Digest::SHA256.new)
     end
-  
-    ssl_bind "0.0.0.0", "3000", {
-      key: key_file.to_path,
-      cert: cert_file.to_path
-    }
+    cert_file.write(root_cert)
   end
-  
+
+  ssl_bind "0.0.0.0", "3000", {
+    key: key_file.to_path,
+    cert: cert_file.to_path
+  }
+end
