c4992fe39e40f993d21c86d981c20462eb4cc2f5
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2025-01-12 04:25:38 +0000

    fix: スタイルを調整

--- app/controllers/admin/card_comments_controller.rb
+++ app/controllers/admin/card_comments_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CardCommentsController < Admin::ApplicationController
   def index
     @status = params[:status]
-    card_comments = CardComment.preload(:card).order(id: :desc)
+    card_comments = CardComment.preload(:card, :user).order(id: :desc)
     card_comments.where!(status: @status) if @status.present?
     @card_comments = card_comments.page(params[:page]).per(100)
   end
--- app/controllers/admin/project_comments_controller.rb
+++ app/controllers/admin/project_comments_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ProjectCommentsController < Admin::ApplicationController
   def index
     @status = params[:status]
-    project_comments = ProjectComment.preload(:project).order(id: :desc)
+    project_comments = ProjectComment.preload(:project, :user).order(id: :desc)
     project_comments.where!(status: @status) if @status.present?
     @project_comments = project_comments.page(params[:page]).per(100)
   end
